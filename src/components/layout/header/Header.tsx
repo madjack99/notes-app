2243f20df7194b87fdf8fc3f7a970f813ddbfb56
Alex <kavanokami@gmail.com>
2020-11-28 12:57:06 +0400

    feat: add header

---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Div = styled.div`
+  padding: 10px 15px;
+  background-color: #a6f8ff;
+  border-bottom: 1px solid black;
+  box-shadow: 0px -2px 10px 1px;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+const H1 = styled.h1`
+  max-width: 75%;
+  margin: 0 auto;
+`;
+
+const Header = () => {
+  return (
+    <Div>
+      <H1>Notes</H1>
+    </Div>
+  );
+};
+
+export default Header;
